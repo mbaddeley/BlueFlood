59ef8ce9481f5affc0f2b787fe2a64d55abcc6d3
Michael Baddeley <michael@ssrc.tii.ae>
2021-08-10 15:47:14 +0400

    Init radio at the start of every round

--- examples/nrf-glossy/dirty-channel.c
+++ examples/nrf-glossy/dirty-channel.c
@@ -256,6 +256,7 @@ PROCESS_THREAD(tx_process, ev, data)
 
   while(1)
   {
+    my_radio_init(&my_id, my_tx_buffer);
     my_radio_gpio_init();
     rx_ok = 0, rx_crc_failed = 0, rx_none = 0; tx_done=0; berr = 0; berr_per_pkt_max = 0, berr_per_byte_max = 0; corrupt_msg_index = 0;
     #if PRINT_TX_STATUS
